bbb2746a70651abd2fffe344bfb91db9dab29ea4
Vonney <davonnejv@gmail.com>
2023-12-12 20:20:15 -0600

    adding text onto image and adding a button

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,7 +1,16 @@
 export default function MainImage() {
   return (
-    <div className="mt-10 max-w-6xl ml-auto mr-auto ">
+    <div className="mt-10 max-w-6xl ml-auto mr-auto relative">
       <img src="images/tub.jpg" />
+      <div className="absolute inset-28 left 3/4 max-w-md text-2xl font-sans p-5 text-blue-900">
+        Since 2019, our talented team works hard everyday to provide the best
+        services for our clients. We customize our offerings based on specific
+        needs, so get in touch today to receive an initial quote.
+        <br />
+        <button className="p-4 mt-10 bg-white rounded-full hover:text-black">
+          Get a Quote
+        </button>
+      </div>
     </div>
   );
 }
