4d62c3c9618bd3777890221d8c26d381e5f2a70c
Vonney <davonnejv@gmail.com>
2023-12-12 20:50:15 -0600

    adding about section and styles to MainImage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import TopHeader from "./components/TopHeader";
 import MainImage from "./components/MainImage";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <TopHeader />
       <MainImage />
+      <About />
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <>
+      <div className="flex items-center justify-center mt-20 text-4xl font-serif">
+        <div className="max-w-5xl flex flex-col">
+          <h2>Our Promise</h2>
+          <p className="text-lg mt-10">
+            Fast, efficient, and honest, Delgado Plumbing & heating has become a
+            reputable and well-known Plumbing Service. Our team is up for every
+            job, managing projects with the skill and experience our clients
+            have come to expect. We want our customers to be satisfied with our
+            work, which is why we provide open communication channels throughout
+            the duration of each project.
+          </p>
+          {/* <img src="images/heating.webp" className="max-w-sm" /> */}
+        </div>
+      </div>
+    </>
+  );
+}
