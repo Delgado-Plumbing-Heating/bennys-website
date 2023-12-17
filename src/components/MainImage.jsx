d03f71240cb92d6185267e2f47407f1f71959838
Vonney <davonnejv@gmail.com>
2023-12-17 14:21:58 -0600

    working on respponsiveness on main image

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,13 +1,13 @@
 export default function MainImage() {
   return (
-    <div className="mt-10 max-w-6xl ml-auto mr-auto relative">
-      <img src="images/tub.jpg" />
-      <div className="absolute inset-24 left 3/4 max-w-md text-2xl font-sans rounded p-10 text-blue-900 bg-white h-min mt-10">
-        Since 2019, our talented team works hard everyday to provide the best
+    <div className="mt-10 max-w-6xl mx-auto relative">
+      <img src="images/tub.jpg" className="w-full" alt="Bathtub" />
+      <div className="md:absolute inset-10  md:inset-24 md:left-14 md:max-w-md resize text-2xl font-sans rounded p-10 text-blue-900 bg-white sm:mt-10 h-min">
+        Since 2019, our talented team works hard every day to provide the best
         services for our clients. We customize our offerings based on specific
         needs, so get in touch today to receive an initial quote.
         <br />
-        <button className=" mt-10 bg-blue-900 rounded-full hover:text-black before:ease relative h-12 w-40 overflow-hidden border border-blue-900 bg-blue-900 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-blue-900 hover:before:-translate-x-40">
+        <button className="mt-10 bg-blue-900 rounded-full hover:text-black before:ease relative h-12 w-40 overflow-hidden border border-blue-900 bg-blue-900 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-blue-900 hover:before:-translate-x-40">
           Get a Quote
         </button>
       </div>
