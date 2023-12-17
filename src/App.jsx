7398562fae4fa1c4a58106edd712cbbf12f926dd
Vonney <davonnejv@gmail.com>
2023-12-17 16:34:55 -0600

    adding second Header with tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import TopHeader from "./components/TopHeader";
+import SecondHeader from "./components/SecondHeader";
 import MainImage from "./components/MainImage";
 import About from "./components/About";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <TopHeader />
+      <SecondHeader />
       <MainImage />
       <About />
     </>
---
+++ src/components/SecondHeader.jsx
@@ -0,0 +1,8 @@
+export default function SecondHeader() {
+  return (
+    <div className="bg-orange-500 w-ful text-white text-center font-serif p-2">
+      Services done in Denver, Westminster, Arvada, Wheat Ridge, Commerice City,
+      Aurora, & Bolder
+    </div>
+  );
+}
