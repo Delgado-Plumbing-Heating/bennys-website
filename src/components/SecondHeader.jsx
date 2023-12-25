dd4454620e9a7aefa70435c0c39321e1a7f942e9
Vonney <davonnejv@gmail.com>
2023-12-24 21:20:05 -0600

    adding phone number to second header

--- src/components/SecondHeader.jsx
+++ src/components/SecondHeader.jsx
@@ -2,7 +2,8 @@ export default function SecondHeader() {
   return (
     <div className="bg-orange-500 w-ful text-white text-center font-serif p-2">
       Services done in Denver, Westminster, Arvada, Wheat Ridge, Commerice City,
-      Aurora, & Bolder
+      Aurora, & Bolder <br />
+      <span className="text-sm ml-10">For More Info Call: 720-877-3362</span>
     </div>
   );
 }
